2312b1af5cf1929072aea95faef8590b16eca7db
Adrian <adekgoik58@gmail.com>
2024-10-21 22:07:08 +0200

    initial commit zero sum game

---
+++ MatrixGame.py
@@ -0,0 +1,19 @@
+/*
+Autorzy: Adrian Goik, Łukasz Soldatke
+Zasady: Gra turowa o sumie zerowej polegająca na wyświetleniu planszy w postaci macierzy NxN,
+której pola przedstawiają punkty o wartościach ujemnych. Każdy z graczy zaczyna z taką samą ilością
+punktów. Należy tak wybierać pola, aby jak najszybciej osiągnąć wartość 0.
+
+Instrukcja przygotowania środowiska:
+
+-komentarze, dokumentacja
+
+*/
+
+from easyAI import ...
+
+def main():
+    '''
+
+    :return:
+    '''
