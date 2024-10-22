caacac0607a02094e566fe6b5d761452f7e2e337
Adrian <adekgoik58@gmail.com>
2024-10-22 21:29:24 +0200

    corrected rules

--- MatrixGame.py
+++ MatrixGame.py
@@ -1,19 +1,17 @@
-/*
+'''
 Autorzy: Adrian Goik, Łukasz Soldatke
-Zasady: Gra turowa o sumie zerowej polegająca na wyświetleniu planszy w postaci macierzy NxN,
-której pola przedstawiają punkty o wartościach ujemnych. Każdy z graczy zaczyna z taką samą ilością
-punktów. Należy tak wybierać pola, aby jak najszybciej osiągnąć wartość 0.
+Zasady: Gra turowa o sumie zerowej polegająca na wyświetleniu planszy w postaci macierzy 5x5,
+której pola posiadają losowe wartości od -10 do 10. Wartości z wybranych pól są dodawane do punktów gracza
+i odejmowane od punktów przeciwnika. Gra kończy się po zajęciu wszystkich pól, a wygrywa gracz z większą liczbą punktów.
 
-Instrukcja przygotowania środowiska:
 
 -komentarze, dokumentacja
 
-*/
+'''
 
-from easyAI import ...
+from easyAI import AI
+import random
 
+def create_matrix():
+    return [[random.randint(-10,10) for _ in range(5)] for _ in range(5)]
 def main():
-    '''
-
-    :return:
-    '''
