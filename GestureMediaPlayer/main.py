fc8dbf16494b367ad536162f5c430f3c18c2b803
lukas <s24729@pjwstk.edu.pl>
2025-01-16 23:45:04 +0100

    Small comments changes

--- GestureMediaPlayer/main.py
+++ GestureMediaPlayer/main.py
@@ -122,7 +122,7 @@ def recognize_gesture(hand_landmarks, frame):
             gesture_start = 0.0
             last_gesture_time = time.time()
 
-    # Następny utwór: Dłoń w pionie
+    # Następny utwór: Wyprostowany palec wskazujący
     elif index_tip.y < index_pip.y:
         if gesture != "next":
             gesture_start = 0.0
@@ -150,7 +150,7 @@ def recognize_gesture(hand_landmarks, frame):
             cv2.putText(frame, 'Previous', (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             gesture_start = 0.0
             last_gesture_time = time.time()
-    # Wyciszenie: Dłoń w poziomie
+    # Wyciszenie: Otwarta dłoń poziomo
     elif pinky_tip.y < pinky_pip.y - 0.02 and pinky_tip.y < index_tip.y and pinky_tip.y < thumb_tip.y:
         if gesture != "mute":
             gesture_start = 0.0
